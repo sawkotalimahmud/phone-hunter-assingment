be1617faa4aa33160c42651b02fbb29e65313719
Neelporos <sawkotalimahmud@gmail.com>
2022-02-28 11:52:50 -0800

    Result Showing

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const searchPhone = () => {
     const searchField = document.getElementById("search-field")
     const searchText = searchField.value;
-    console.log(searchText);
 
     searchField.value = "";
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data))
+}
 
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting  text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+            </div>
+        </div>`;
+        searchResult.appendChild(div);
+    });
 }
