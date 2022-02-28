f05053c55bf1cd274d93789d673027881c1f2264
Neelporos <sawkotalimahmud@gmail.com>
2022-02-28 12:25:33 -0800

    brack

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const displaySearchResult = phones => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div class="card ">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">This is a longer card with supporting  text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+                <p class="card-text">${phone.brand}</p>
+                <h4 class="card-title">${phone.phone_name}</h4>
+                <button type="button" class="btn btn-outline-primary">Details</button>
             </div>
         </div>`;
         searchResult.appendChild(div);
